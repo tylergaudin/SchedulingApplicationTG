53c9fb8441cd1fb00d95e0a81317f7bae2a2f7d7
Tyler Gaudin <tylermerrek@gmail.com>
2023-03-09 17:31:55 -0700

    Second save

--- src/appmain/Main.java
+++ src/appmain/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
